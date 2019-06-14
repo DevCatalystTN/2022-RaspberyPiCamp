130bae1cdc6805f449edc5ee26a03e4075cb9277
Dev Catalyst <44650216+DevCatalystTN@users.noreply.github.com>
2019-06-14 12:33:45 -0400

    Add files via upload

---
+++ explorerhat-touch-motors.py
@@ -0,0 +1,40 @@
+#will light a different light for each numbered button
+import explorerhat
+from time import sleep
+from gpiozero import Button
+
+def redOn(channel, event):  #turns on red LED and motor forward
+    explorerhat.light[2].on()
+    explorerhat.motor.one.forward(25)
+
+def blueOn(channel, event): #turns on blue LED and motor backward
+    explorerhat.light[0].on()
+    explorerhat.motor.one.backward(25)
+
+def blueOff(channel, event):    #reverse of blueOn
+    explorerhat.light[0].off()
+    explorerhat.motor.one.stop()
+
+def redOff(channel, event):     #reverse of redOn
+    explorerhat.light[2].off()
+    explorerhat.motor.one.stop()
+
+
+
+while True:    #infinite loop
+    explorerhat.touch.one.pressed(blueOn)
+    #returns true when the 1 button is pressed and runs blueOn
+
+    explorerhat.touch.three.pressed(redOn)
+    #returns true when the 3 button is pressed and runs redOn
+
+    explorerhat.touch.one.released(blueOff)
+    #returns true when the 1 button is released and runs blueOff
+
+    explorerhat.touch.three.released(redOff)
+    #returns true when the 3 button is released and runs redOff
+        
+
+    
+
+    
