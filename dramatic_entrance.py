c0c6731adbbd743d487e5255400379e2bde12a6e
Dev Catalyst <44650216+DevCatalystTN@users.noreply.github.com>
2019-06-18 14:38:10 -0400

    Add files via upload

---
+++ dramatic_entrance.py
@@ -0,0 +1,12 @@
+from gpiozero import MotionSensor#imports motion sensor
+import pygame.mixer#imports the pygame instance library
+
+sensor = MotionSensor(5)#shortens the sensor syntax
+
+pygame.init()#begins an instance of pygame
+sound  = pygame.mixer.Sound('/home/pi/doorbell-1.wav')
+#sets the sound to be played
+
+while True:#infinite loop
+    if sensor.motion_detected:#checks for motion
+        sound.play()#plays the specified noise
